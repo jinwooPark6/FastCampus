884bc21bc1d58fa409aa7212d7d9f02900998702
jinwooPark6 <jinwoo13245@naver.com>
2021-09-08 03:33:38 +0900

    자바 기초

---
+++ Chapter1/src/ch03/VariableTest.java
@@ -0,0 +1,16 @@
+package ch03;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		
+		byte bnum = -128;
+		
+		//int num = 12345678900;
+		long lnum = 12345678900L;
+		
+		System.out.println(bnum);
+		System.out.println(lnum);
+		
+	}
+}
